e7ab20c343ea0c3d427b8dba6e98a47df3e690e2
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-14 19:48:15 +0530

    updated calender.py

--- src/kutty/connect.py
+++ src/kutty/connect.py
@@ -1,11 +1,4 @@
 """ Using Langchain Going to Generate a Server that can be used to shcedule meetings """
 # Importing the required libraries
+import os
 import langchain
-import langchain_google_meet
-
-
-# Create a Langchain LLM server.
-server = langchain.Server()
-
-# Create a Langchain Google Meet client.
-client = langchain_google_meet.Client(server)
