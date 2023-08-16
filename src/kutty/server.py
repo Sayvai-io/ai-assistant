84dd10a9d10cac61621891fa3a385be8eb80b9bd
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-16 20:12:51 +0530

    updated server file

--- src/kutty/server.py
+++ src/kutty/server.py
@@ -2,11 +2,11 @@
 # Importing the required libraries
 import os
 import langchain
-from pydantic import BaseModel
+# from langchain.chat_models import ChatOpenAI
+from typing import List, Dict, Any
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
 from langchain.llms import OpenAI
-from agents.agentloader import AgentLoader
-from tools.toolloader import ToolLoader
-from langchain.chat_models import ChatOpenAI
 
 
 with open("../../openai_api_key.txt", "r") as f:
@@ -18,32 +18,31 @@ with open("../../serp_api_key.txt", "r") as f:
 os.environ["OPENAI_API_KEY"] = OPEN_AI_KEY
 os.environ["SERPAPI_API_KEY"] = SERP_API_KEY
 
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+# llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
 
-class LangchainServer:
-    def __init__(self):
-        self.llm = llm
-        self.tools = ["serpapi", "llm-math"]
-        self.agent = None
+class LlmServer:
+    """LLm server class"""
+    def __init__(self) -> None:
+        self.llm = OpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+        self.tools = None
+        self.agent = None     
+        
     
-    def load_agent_and_tools(self, agent_name: str, tool_names: str) -> str:
-        # loading agents
-        tool_object = ToolLoader(tool_names, llm=self.llm)
-        self.tools = tool_object.get_tools()
-        agent_object = AgentLoader(tools=self.tools, llm=llm, agent_name=agent_name)
-        self.agent = agent_object.get_agent()
-        return "Agent Loaded Successfully"
+    def initialize(self, tools: List, agent: str) -> str:
+        """Initialize the server"""
+        if self.tools is None:
+            self.tools = load_tools(tools, llm=self.llm)
+        self.agent = initialize_agent(self.tools, self.llm, agent, verbose=False)
+        return "Initialized"
     
-    def get_response(self, message: str) -> str:        
-        # This function is used to get the response from the agent 
-        if self.agent is None:
-            raise Exception("Agent Not Loaded")
-        return self.agent.run(message)
+    def get_response(self, text: str) -> Dict[str, Any]:
+        response = self.agent.run(text)
+        return response
+    
 
 if __name__ == "__main__":
-    server = LangchainServer()
-    server.load_agent_and_tools(agent_name="zero-shot-react-description", tool_names=["serpapi", "llm-math"])
-    print(server.get_response("What is the square root of 4?"))
-    
+    server = LlmServer()
+    server.initialize(["serpapi", "llm-math"], "zero-shot-react-description")
+    print(server.get_response("What is the weather today?"))
 
 
